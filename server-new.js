5a3ec34d55a35626e459445bfd16076fdd9ae11c
Steph Hanglan <Stephen.hanglan@gmail.com>
2026-09-24 12:51:57 +0200

    Scrub ETL and ended ECA from citizen benefits guide; add Render route.

--- server-new.js
+++ server-new.js
@@ -205,6 +205,7 @@ app.get('/greenways/energy-monitoring-references', (req, res) => sendLiveMusicHt
 app.get('/greenways/renovation-plans', (req, res) => sendHtmlsFile(res, 'Renovation project plans.html'));
 app.get('/greenways/eco-project-planner', (req, res) => sendHtmlsFile(res, 'eco_project_planning_guide_fixed.html'));
 app.get('/greenways/low-energy-equipment-savings', (req, res) => sendHtmlsFile(res, 'Low-energy-equipment saving.html'));
+app.get('/greenways/citizen-benefits', (req, res) => sendHtmlsFile(res, 'Citizen benefits Guide 2 update.html'));
 app.get('/greenways/insulation-guide', (req, res) => sendLiveMusicHtml(res, 'Insulation .html'));
 app.get('/greenways/finance-prices-board', (req, res) => sendLiveMusicHtml(res, 'greenways-finance-prices-board.html'));
 app.get('/greenways/finance-prices-board-embed', (req, res) => sendLiveMusicHtml(res, 'greenways-finance-prices-board.html'));
